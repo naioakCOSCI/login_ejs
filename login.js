622a5d565d7f00dbf7ea8f6a1140e04be48c6df4
Sittichai Worachotekamjorn <naioak@yahoo.com>
2021-10-01 14:57:43 +0700

    First login Nodejs

---
+++ login.js
@@ -0,0 +1,8 @@
+// 1.include the packages in our Node.js
+var mysql = require("mysql");
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
